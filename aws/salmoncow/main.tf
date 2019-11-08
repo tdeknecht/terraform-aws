ab16821c274975f9aa352b189b09254af27ec19c
themicrohawk <themicrohawk@gmail.com>
2019-11-07 21:06:54 -0600

    modularized

--- aws/salmoncow/main.tf
+++ aws/salmoncow/main.tf
@@ -7,30 +7,14 @@ provider "aws" {
 locals {
     name = "salmoncow"
 
-    private_subnets = {
-        "us-east-1a" = "10.0.1.0/24",
-        "us-east-1b" = "10.0.2.0/24"
-    }
-
-    public_subnets = {
-        "us-east-1a" = "10.0.3.0/24",
-        "us-east-1b" = "10.0.4.0/24"
-    }
-
     tags = {
         deployment  = "terraform"
     }
 }
-/*
-resource "aws_vpc" "salmoncow" {
-    cidr_block = "10.0.0.0/16"
 
-    tags = {
-        Name = local.name,
-        deployment = "terraform"
-    }
-}
-*/
+# ******************************************************************************
+# Create vpc
+# ******************************************************************************
 module "vpc" {
     source = "../modules/vpc/"
 
@@ -40,13 +24,23 @@ module "vpc" {
     tags = local.tags
 }
 
+# ******************************************************************************
+# Create subnets
+# ******************************************************************************
 module "subnets" {
     source = "../modules/subnets/"
 
     vpc_id = module.vpc.vpc_id
 
-    private_subnets = local.private_subnets
-    public_subnets  = local.public_subnets
+    private_subnets = {
+        "us-east-1a" = "10.0.1.0/24",
+        "us-east-1b" = "10.0.2.0/24"
+    }
+
+    public_subnets = {
+        "us-east-1a" = "10.0.3.0/24",
+        "us-east-1b" = "10.0.4.0/24"
+    }
 
     name = local.name
     tags = local.tags
