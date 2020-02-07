b6989ed3bd47c277995aecfa0317c3837e2955d0
themicrohawk <themicrohawk@gmail.com>
2020-02-06 21:15:50 -0600

    m

--- aws/sandbox/main.tf
+++ aws/sandbox/main.tf
@@ -6,6 +6,23 @@ provider "aws" {
     profile = "default"
 }
 
+variable cgw_configs { 
+    default = {
+        cgw-id001 = [ 
+            ["10.0.0.1/30", "10.0.0.2/30"], # Tunnel pair 1
+            ["10.0.0.2/30", "10.0.0.3/30"], # Tunnel pair 2
+            ["10.0.0.4/30", "10.0.0.5/30"], # Tunnel pair 3
+            ["10.0.0.6/30", "10.0.0.7/30"], # Tunnel pair 4
+        ],
+        cgw-id002 = [ 
+            ["10.0.0.0/30", "10.0.0.1/30"], # Tunnel pair 1
+            ["10.0.0.2/30", "10.0.0.3/30"], # Tunnel pair 2
+            ["10.0.0.4/30", "10.0.0.5/30"], # Tunnel pair 3
+            ["10.0.0.6/30", "10.0.0.7/30"], # Tunnel pair 4
+        ],
+    }
+}
+
 locals {
     name = "sandbox"
 
@@ -19,33 +36,47 @@ locals {
 
 
     # ---------------------------------------------------
-    build_vpn = true
+    build_vpn = true # get rid of this. the for_each won't run if you pass it an empty map
+
+    listmap = [
+        for cgw_id, tunnels in var.cgw_configs: {
+            for index, ips in tunnels: 
+                    format("%s-vpntunnel-%s", cgw_id, index) =>
+                        length(ips) % 2 == 0 ? [ format("%s", cgw_id) , ips     ] # If two IPs are passed in, use them
+                                             : [ format("%s", cgw_id) , ["",""] ] # Else create two null IPs and hope for the best
+        }
+    ]
+
+
+    /*
     cgw_ids = ["cgw-id001", "cgw-id002"]
+    cgw1_vpn_tunnel1_ips = ["10.0.0.0", "10.0.0.2", "10.0.0.4", "10.0.0.6"]
+    cgw1_vpn_tunnel2_ips = ["10.0.0.4", "10.0.0.6", "10.0.0.5", "10.0.0.7"]
+
+    cgw2_vpn_tunnel1_ips = ["10.0.1.0", "10.0.1.2", "10.0.1.4", "10.0.1.6"]
+    cgw2_vpn_tunnel2_ips = ["10.0.1.1", "10.0.1.3", "10.0.1.5", "10.0.1.7"]
+
     vpn_connections = 4
+    #vpn_connections = length(local.cgw1_vpn_tunnel1_ips)
 
     listmap = local.build_vpn ? [
-        for cgw in local.cgw_ids: {
+        for index, cgw in local.cgw_ids: {
             for vpns in range(local.vpn_connections):
-                format("%s-vpn-%s", cgw, vpns) => cgw
+                format("%s-vpn-%s", cgw, vpns) => 
+                    index == 0 ? [ format("%s", cgw), [local.cgw1_vpn_tunnel1_ips[vpns], local.cgw1_vpn_tunnel2_ips[vpns]] ]
+                               : [ format("%s", cgw), [local.cgw2_vpn_tunnel1_ips[vpns], local.cgw2_vpn_tunnel2_ips[vpns]] ]
         }
     ] : []
-
-    map = merge(local.listmap...)
-
-    /* OUTPUTS:
-    map = {
-        "cgw-id001-vpn-0" = "cgw-id001"
-        "cgw-id001-vpn-1" = "cgw-id001"
-        "cgw-id001-vpn-2" = "cgw-id001"
-        "cgw-id001-vpn-3" = "cgw-id001"
-        "cgw-id002-vpn-0" = "cgw-id002"
-        "cgw-id002-vpn-1" = "cgw-id002"
-        "cgw-id002-vpn-2" = "cgw-id002"
-        "cgw-id002-vpn-3" = "cgw-id002"
-    }   
     */
-    # ---------------------------------------------------
+
+    
+    # Need to use this until the bug is fixed where input variables have an invalid type.
+    # Once the bug is fixed, use this:   map = merge(local.listmap...)
+    map = length(local.listmap) == 2 ? merge(local.listmap[0], local.listmap[1]) : merge(local.listmap[0])
 }
 
 #output "nat_routes" { value = local.nat_routes }
+
+#output "listmap" { value = local.listmap }
+
 output "map" { value = local.map }
