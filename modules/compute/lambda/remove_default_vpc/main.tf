45b5c06808e315d16c31c74290c7f25ed8052888
tdeknecht <tdeknecht@gmail.com>
2020-09-20 20:08:48 -0500

    adding tags to remove_default_vpc

--- modules/compute/lambda/remove_default_vpc/main.tf
+++ modules/compute/lambda/remove_default_vpc/main.tf
@@ -10,21 +10,20 @@ data "archive_file" "lambda_function" {
 
 # remove_default_vpc
 resource "aws_lambda_function" "remove_default_vpc" {
-  filename      = "${path.module}/remove_default_vpc.zip"
-  function_name = "remove_default_vpc"
-  description   = "Remove the Default VPC in all AWS Regions"
-  role          = aws_iam_role.remove_default_vpc_role.arn
-  handler       = "remove_default_vpc.lambda_handler"
-
+  filename         = "${path.module}/remove_default_vpc.zip"
+  function_name    = "remove_default_vpc"
+  description      = "Remove the Default VPC in all AWS Regions"
+  role             = aws_iam_role.remove_default_vpc_role.arn
+  handler          = "remove_default_vpc.lambda_handler"
   source_code_hash = data.archive_file.lambda_function.output_base64sha256
-
-  runtime = "go1.x"
+  runtime          = "go1.x"
+  tags             = var.tags
 }
 
 resource "aws_lambda_alias" "remove_default_vpc_alias" {
-    name = "${var.ou}_v1"
-    function_name = aws_lambda_function.remove_default_vpc.arn
-    function_version = aws_lambda_function.remove_default_vpc.version
+  name             = "${var.ou}_v1"
+  function_name    = aws_lambda_function.remove_default_vpc.arn
+  function_version = aws_lambda_function.remove_default_vpc.version
 }
 
 # ------------------------------------------------------------------------------
@@ -34,8 +33,8 @@ resource "aws_lambda_alias" "remove_default_vpc_alias" {
 # policy doc
 data "aws_iam_policy_document" "remove_default_vpc_policy" {
   statement {
-    sid     = "removeDefaultVpcRole"
-    effect  = "Allow"
+    sid    = "removeDefaultVpcRole"
+    effect = "Allow"
     actions = [
       "ec2:*",
     ]
@@ -45,6 +44,6 @@ data "aws_iam_policy_document" "remove_default_vpc_policy" {
 
 # role
 resource "aws_iam_role" "remove_default_vpc_role" {
-  name = "removeDefaultVpcRole"
+  name               = "removeDefaultVpcRole"
   assume_role_policy = data.aws_iam_policy_document.remove_default_vpc_policy.json
 }
