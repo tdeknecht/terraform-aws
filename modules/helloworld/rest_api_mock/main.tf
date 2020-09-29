76f3e11cc26dbf18a525ea3ec5ddbeba62d4cda8
tdeknecht <tdeknecht@gmail.com>
2020-09-29 18:26:34 -0500

    updates to how mock api works

--- modules/helloworld/rest_api_mock/main.tf
+++ modules/helloworld/rest_api_mock/main.tf
@@ -8,39 +8,59 @@ data "aws_region" "current" {}
 
 data "aws_iam_account_alias" "current" {}
 
-locals {
-  endpoint_configuration_no_vpc = length(var.vpc_endpoint_ids) == 0 ? toset([var.type]) : toset([])
-
-  endpoint_configuration_vpc = length(var.vpc_endpoint_ids) > 0 ? {
-    (var.type) = var.vpc_endpoint_ids
-  } : {}
-}
-
 # ------------------------------------------------------------------------------
 # API Gateway: API
 # ------------------------------------------------------------------------------
+
+data "aws_vpc_endpoint" "execute_api" {
+  count  = length(var.vpc_endpoint_ids) > 0 ? 1 : 0
+
+  filter {
+    name = "vpc-endpoint-id"
+    values = var.vpc_endpoint_ids
+  }
+}
+
+data "aws_iam_policy_document" "private_api" {
+  count  = length(var.vpc_endpoint_ids) > 0 ? 1 : 0
+
+  statement {
+    sid       = "invokeApi"
+    effect    = "Allow"
+    actions   = ["execute-api:Invoke"]
+    resources = ["execute-api:/*"]
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+  }
+  statement {
+    sid       = "invokeApi"
+    effect    = "Deny"
+    actions   = ["execute-api:Invoke"]
+    resources = ["execute-api:/*"]
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    condition {
+      test = "StringNotEquals"
+      variable = "aws:SourceVpc"
+      values = [data.aws_vpc_endpoint.execute_api[0].vpc_id]
+    }
+  }
+}
 
 resource "aws_api_gateway_rest_api" "api" {
   name        = "${var.ou}-${data.aws_iam_account_alias.current.account_alias}-${data.aws_region.current.name}-${var.name}-${var.type}-api"
   description = "A ${var.type} Hello World API with MOCK integration"
   tags        = var.tags
 
-  dynamic "endpoint_configuration" {
-    for_each = local.endpoint_configuration_no_vpc
-
-    content {
-      types = [upper(endpoint_configuration.value)]
-    }
+  endpoint_configuration {
+    types = [var.type]
+    vpc_endpoint_ids = length(var.vpc_endpoint_ids) > 0 ? var.vpc_endpoint_ids : null
   }
-
-  dynamic "endpoint_configuration" {
-    for_each = local.endpoint_configuration_vpc
-
-    content {
-      types            = [upper(endpoint_configuration.key)]
-      vpc_endpoint_ids = endpoint_configuration.value
-    }
-  }
+  policy = length(var.vpc_endpoint_ids) > 0 ? data.aws_iam_policy_document.private_api[0].json : null
 }
 
 resource "aws_api_gateway_resource" "resource" {
@@ -144,9 +164,10 @@ resource "aws_api_gateway_deployment" "deployment" {
 
   # redeploy if code changes
   triggers = {
-    redeployment = sha1(join(",", list(
-      jsonencode(aws_api_gateway_integration.get),
-    )))
+    # redeployment = sha1(join(",", list(
+    #   jsonencode(aws_api_gateway_integration.get),
+    # )))
+    redeployment = sha1(file("main.tf"))
   }
 
   # reduce downtime by recreating before destroying
@@ -163,4 +184,8 @@ resource "aws_api_gateway_stage" "stage" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   deployment_id = aws_api_gateway_deployment.deployment.id
   tags          = var.tags
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
