99d2545c4ff356f357610884c1d2b08a6576818e
tdeknecht <tdeknecht@gmail.com>
2020-10-03 19:08:29 -0500

    s3 bucket output update

--- modules/storage/s3_bucket/outputs.tf
+++ modules/storage/s3_bucket/outputs.tf
@@ -8,6 +8,11 @@ output "id" {
 }
 
 output "arn" {
-  description = "The ARN of the bucket. Will be of format arn:aws:s3:::bucketname."
+  description = "The ARN of the bucket. Will be of format `arn:aws:s3:::bucketname`."
   value       = aws_s3_bucket.s3_bucket.arn
+}
+
+output "bucket_domain_name" {
+  description = "The bucket domain name. Will be of format `bucketname.s3.amazonaws.com`."
+  value       = aws_s3_bucket.s3_bucket.bucket_domain_name
 }
