e321f6f7b287bcfcf70406573d9772f30a4beda0
tdeknecht <tdeknecht@gmail.com>
2020-10-05 17:05:53 -0500

    added s3 output

--- modules/storage/s3_bucket/outputs.tf
+++ modules/storage/s3_bucket/outputs.tf
@@ -16,3 +16,8 @@ output "bucket_domain_name" {
   description = "The bucket domain name. Will be of format `bucketname.s3.amazonaws.com`."
   value       = aws_s3_bucket.s3_bucket.bucket_domain_name
 }
+
+output "hosted_zone_id" {
+  description = "The Route 53 Hosted Zone ID for this bucket's region."
+  value       = aws_s3_bucket.s3_bucket.hosted_zone_id
+}
