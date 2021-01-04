b602955e6f8a8b5036d48f4c981fe248bbe7f563
tdeknecht <tdeknecht@gmail.com>
2021-01-04 17:12:06 -0600

    so answer

--- dev/.terraform.lock.hcl
+++ dev/.terraform.lock.hcl
@@ -7,3 +7,37 @@ provider "registry.terraform.io/hashicorp/aws" {
     "h1:3M0q/GqB24RM5E1wIu23bEbChJ2RDVaM5lTLENB92Cc=",
   ]
 }
+
+provider "registry.terraform.io/hashicorp/http" {
+  version = "2.0.0"
+  hashes = [
+    "h1:PZBhWKq5UZDP0sv8lbR7D29dYcpnPrQXiXzunA/+zI4=",
+    "zh:066b5f4f80013dcc7d94d7d4b5537607c74f93e8a414ac993709d138a36d7db6",
+    "zh:41c1916d44c263828d39eeb12ab0bea8507e4e41af6f3376cb3f2b93158e35a6",
+    "zh:78a429752ae9c3587a4ccf106ea17a89918732511f45f99ec905d014f88a3e88",
+    "zh:8be68e5e4f095a090534594d1e0e08a8ff76638fc94cb38e5e38a683b8cb62ec",
+    "zh:b00a3d86b86ac07347cd3d89f8bbb966686d897a7c37a18a69b715e8e01a4728",
+    "zh:b9aab522396439716fb2e2cacf97c4083835fd0f8ea15b95d0a754578253c66c",
+    "zh:c475bfacb7da999ff5a652680e7a7969b63c1e994462936a1ac7b98708e4ea1f",
+    "zh:d85f228d5fb4d4197972939d27a00cc6f48309a3d562de44eade347c70f02f83",
+    "zh:d91ece204cd2aec195dc7e0d1a620ceb19dcc2a64072af134fc9e49f233abb3e",
+    "zh:faa5c9801b6686872908fb55d5feb4952168dd53e0dadec0962768e24b93116a",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/kubernetes" {
+  version = "1.13.3"
+  hashes = [
+    "h1:whoGs/NeucMF8U/urPaeXdQUb+ppaO1Ae4r5aJRhfrU=",
+    "zh:11fd58df9c297a4f3cab82ce8eb2f54f1be27f78fa23be2273ecd545ab254b91",
+    "zh:5b9e6f352c5666d791e2658a1d18bf0990f3ab70c99c916c393a2ee7f385364c",
+    "zh:5c94f1350471a5c8e8ee6675874608c506a0bfd3164bdd91b802842723547e2c",
+    "zh:5d9c5c44dba9addbb86491339012096e74778bb4ea93b70f12333bffba3d05e6",
+    "zh:6336f9cbb0b580f247cebb97fb7d4cc5e7fe9cc734d8d958d84c4ea3f1e24041",
+    "zh:bca3b9d4dcbe6f804f5611a83add371dc03b5aa92271f60ebdc2216bfedfab28",
+    "zh:cbcdc87a593090f490f7899f4f2d302e0c7023155591fcf65e6fadd69f5452f0",
+    "zh:ec2886a1adbfe3c861b2deb9446369111b9c6116701ae73ef372dc7df5bb3c9e",
+    "zh:edb5b4172610672bb4d7425511961fda2047b8a00675b99ae6887cd2ece4bda9",
+    "zh:ff7ea7743246181ea739643d7751c37041c4016eb6bbc39beb1e3b4e99629112",
+  ]
+}
--- dev/main.tf
+++ dev/main.tf
@@ -4,6 +4,85 @@ provider "aws" {
   profile = "default"
 }
 
+# ------------------------------------------------------------------------------
+# stack overflow: https://stackoverflow.com/questions/65569177/how-to-iterate-through-nested-list-of-objects-in-terraform
+# ------------------------------------------------------------------------------
+
+# locals {
+#   urls = toset(flatten([
+#     for cm in var.cloud-configmap : [
+#       for cm-file in cm.cm-files :
+#         format("https://%s", cm-file.url)
+#     ]
+#   ]))
+# }
+# output "urls" { value = local.urls }
+
+# variable "cloud-configmap" {
+#   type = map(object({
+#     name = string
+#     namespace = string
+#     cm-files = list(object({
+#       url = string
+#       data-keyname = string 
+#     }))
+#   }))
+#   default = {
+#     "cm1" = {
+#       name = "cm-name"
+#       namespace = "testnamespace"
+#       cm-files = [{
+#         url = "someurl.com/file1.yaml"
+#         data-keyname = "file1.yml"
+#       },
+#       {
+#         url = "someurl.com/file2.yaml"
+#         data-keyname = "file2.yml"
+#       }]
+#     },
+#     "cm2" = {
+#       name = "cm-name2"
+#       namespace = "default"
+#       cm-files = [{
+#         url = "someurl.com/file3.yaml"
+#         data-keyname = "file3.yml"
+#       },
+#       {
+#         url = "someurl.com/file4.yaml"
+#         data-keyname = "file4.yml"
+#       }]
+#     }
+#   }
+# }
+
+# data "http" "config-map" {
+#   for_each = toset(flatten([
+#     for cm in var.cloud-configmap : [
+#       for cm-file in cm.cm-files :
+#         cm-file.url
+#     ]
+#   ]))
+
+#   url = format("https://%s", each.key)
+
+#   request_headers = {
+#     Accept = "text/plain"
+#   }
+# }
+
+# resource "kubernetes_config_map" "configmap" {
+#   for_each = var.cloud-configmap
+
+#   metadata {
+#     name = each.value.name
+#     namespace = each.value.namespace
+#   }
+
+#   data = {
+#     for cm-file in each.value.cm-files :
+#       cm-file.url => cm-file.data-keyname
+#   }
+# }
 
 
 # ------------------------------------------------------------------------------
