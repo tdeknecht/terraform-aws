6956c87083f8818899a7074f19edd243b7fc4b6f
tdeknecht <tdeknecht@gmail.com>
2022-11-04 09:13:35 -0500

    feat: migrating to scalr

--- dev/backend.tf
+++ dev/backend.tf
@@ -4,22 +4,5 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.55.0"
     }
-    tfe = {
-      version = "~> 0.27.0"
-    }
   }
-
-  # requires `terraform login` or CLI config-file:credentials block (https://www.terraform.io/cli/config/config-file#credentials)
-  cloud {
-    organization = "tdeknecht-org"
-
-    workspaces {
-      tags = ["td000", "dev"]
-    }
-  }
-}
-
-resource "tfe_organization" "tdeknecht" {
-  name  = "tdeknecht-org"
-  email = "tdeknecht@gmail.com"
 }
--- dev/providers.tf
+++ dev/providers.tf
@@ -1,8 +1,3 @@
 provider "aws" {
   region  = var.region
-  profile = "default"
-}
-
-provider "tfe" {
-  token = var.tfe_user_token
 }
