6c0416c0c8fb358622a662ae5e80973f79ec400b
tdeknecht <tdeknecht@gmail.com>
2022-11-04 14:21:17 -0500

    test: testing

--- dev/providers.tf
+++ dev/providers.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region  = var.region
 
   assume_role {
-    role_arn = "arn:aws:iam::919814621061:user/scalr-saas"
+    role_arn    = "arn:aws:iam::919814621061:user/scalr-saas"
+    external_id = "BhgYExxs4u5fibK4"
   }
 }
