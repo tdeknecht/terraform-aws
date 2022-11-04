13b6ce3956ee90f52961f03cc520a881e7b6977f
tdeknecht <tdeknecht@gmail.com>
2022-11-04 14:11:42 -0500

    test: testing

--- dev/providers.tf
+++ dev/providers.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region  = var.region
 
   assume_role {
-    role_arn     = "arn:aws:iam::678856875282:role/ScalrIntegrationRole"
+    role_arn     = "arn:aws:iam::678856875282:role/td001-dev-scalr-deployer-role"
   }
 }
